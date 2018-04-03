c0800525b3a75266e4f6c662616340d4c8918a0c
Vladimir Mikhalev <vladimirmsm@yandex.ru>
2018-04-03 10:21:59 +0300

    Created search fragment layout

---
+++ app/src/main/java/io/github/vladimirmi/localradio/SectionsPagerAdapter.java
@@ -0,0 +1,26 @@
+package io.github.vladimirmi.localradio;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new SearchFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
