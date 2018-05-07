6aa5601b4adf174c50c530276b7b3072431f25cf
Vladimir Mikhalev <vladimirmsm@yandex.ru>
2018-05-07 20:49:06 +0300

    fix: Remove the recycler view item decorator on the favorites page

--- app/src/main/java/io/github/vladimirmi/localradio/presentation/favorite/FavoriteFragment.java
+++ app/src/main/java/io/github/vladimirmi/localradio/presentation/favorite/FavoriteFragment.java
@@ -7,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -60,9 +59,6 @@ public class FavoriteFragment extends BaseFragment<FavoritePresenter>
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         stationList.setLayoutManager(layoutManager);
-        DividerItemDecoration itemDecoration = new DividerItemDecoration(stationList.getContext(),
-                layoutManager.getOrientation());
-        stationList.addItemDecoration(itemDecoration);
         stationsAdapter = new StationsAdapter(this);
         stationList.setAdapter(stationsAdapter);
     }
